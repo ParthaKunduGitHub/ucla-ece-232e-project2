87735585f9de67aefcc4d998dbc4a9e269be742d
ParthaKunduGitHub <mr.pkundu@gmail.com>
2024-08-21 14:09:38 -0700

    adding initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ucla-ece-232e-project2",
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "jupyter",
+        "notebook"
+    ]
+)
